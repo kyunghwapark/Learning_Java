76b388cbfe4c0f052f3ba9fbf3ac50edb2d34a5c
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-22 20:45:03 +0900

    01. 자바 프로그래밍 시작하기
    학습도서 : Do it! 자바프로그래밍 (by. 박은종)

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,14 @@
+/*
+ * Date : 2019.01.22
+ * Author : KyunhHwa Park
+ * Description : 자바 복습(Do it! 자바프로그래밍 입문) 중 만든 첫 번째 자바 프로그램입니다.
+ */
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// 한 줄 주석 연습입니다.
+		System.out.println("Hello, Java");
+	}
+}
---
+++ First/src/hello/Quiz1.java
@@ -0,0 +1,15 @@
+/*
+ * 01. 자바 프로그래밍 시작하기
+ * 연습문제 Q6.
+ */
+package hello;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		// 내 이름 출력하기
+		System.out.println("연습문제 6. 내 이름 출력하기");
+		System.out.println("My name is KyungHwa Park");
+	}
+
+}
