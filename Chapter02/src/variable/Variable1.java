773966b22907b69a17fa1066050a99e233bd27a4
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-22 21:46:19 +0900

    02. 변수와 자료형
    학습도서 : Do it! 자바프로그래밍 (by. 박은종)

---
+++ Chapter02/src/variable/Variable1.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int level;						// 정수형 변수 level을 선언
+		level = 10;						// 변수 level에 값 10을 대입
+		System.out.println(level);		// level값 출력
+		
+		
+		// 1분 복습 문제
+		int age;						// 나이 값을 담을 정수형 변수 age를 선언
+		age = 33;						// 변수 age에 값 대입
+		System.out.println(age);		// age값 출력
+	}
+
+}
---
+++ Chapter02/src/variable/Variable2.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int level = 10;					// level 변수 선언과 동시에 값을 대입(초기화)
+		System.out.println(level);		// level값 출력
+		
+		
+		// 1분 복습 문제
+		// Q) 'int형 변수 year에 값 2018을 대입한다'를 의미하는 코드를 완성해 보세요.
+		int year = 2018;
+	}
+
+}
