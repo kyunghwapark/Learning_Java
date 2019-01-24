9b5832f638230ad830134f71a732b9c54abd6327
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-24 17:02:28 +0900

    02. 변수와 자료형 연습문제
    학습도서 : Do it! 자바프로그래밍 (by. 박은종)

---
+++ Chapter02/src/exercise/Q4.java
@@ -0,0 +1,30 @@
+package exercise;
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		int var1 = 10;
+		double var2 = 2.0;
+		
+		
+		// 사칙 연산 결과를 정수로 출력
+		int add = (int)(var1 + var2);
+		System.out.println(add);
+		
+		int sub = (int)(var1 - var2);
+		System.out.println(sub);
+		
+		int mul = (int)(var1 * var2);
+		System.out.println(mul);
+		
+		int div = (int)(var1 / var2);
+		System.out.println(div);
+
+		System.out.println((int)(var1 + var2));
+		System.out.println((int)(var1 - var2));
+		System.out.println((int)(var1 * var2));
+		System.out.println((int)(var1 / var2));
+
+	}
+
+}
---
+++ Chapter02/src/exercise/Q5.java
@@ -0,0 +1,14 @@
+package exercise;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		char ch1 = '글';
+		System.out.println((int)ch1);		// '글'이라는 한글 문자의 유니코드 값 찾기
+		
+		char ch2 = 44544;
+		System.out.println(ch2);			// 유니코드 값으로 문자 출력하기
+
+	}
+
+}
