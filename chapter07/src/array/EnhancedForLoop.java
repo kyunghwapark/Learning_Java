e40fcf16e856411a57035c20d6090f8fa5220568
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-30 20:14:53 +0900

    07-1. 배열이란? : 향상된 for문
    chapter 07. 배열과 ArrayList

---
+++ chapter07/src/array/EnhancedForLoop.java
@@ -0,0 +1,20 @@
+package array;
+
+/*
+ * Quiz) 1분 복습
+ * 향상된 for문
+ */
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		
+		int[] numArray = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		for(int number : numArray) {
+			System.out.print(number + " ");
+		}
+
+	}
+
+}
